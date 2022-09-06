57bd4d08b12826f1a9137376b73cbe4be0aaef0f
Fabiano Novais <fabiano.novais.99@gmail.com>
2022-09-06 13:25:14 -0300

    Conversor de Dollar para Real com orientação da video aula concluído

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function Converter() {
     var elemento = document.getElementById("valor");
     var valor = elemento.value;
-    console.log(valor);
+    var valorEmDolar = parseFloat(valor);
+
+    var valorEmReal = valorEmDolar * 5.43;
+
+    var elementValue = document.getElementById("valorConvertido");
+    var valorConvertido = "O resultado em real é " + valorEmReal;
+
+    elementValue.innerHTML = valorConvertido;
 }
